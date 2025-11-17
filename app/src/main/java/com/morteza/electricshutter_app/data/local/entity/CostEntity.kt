59e16fd140e8744dfdefad2405d5383736069214
mortezanavazesh-coder <morteza.navazesh@gmail.com>
2025-11-17 18:36:56 +0330

    Update CostEntity.kt

--- app/src/main/java/com/morteza/electricshutter_app/data/local/entity/CostEntity.kt
+++ app/src/main/java/com/morteza/electricshutter_app/data/local/entity/CostEntity.kt
@@ -5,8 +5,8 @@ import androidx.room.PrimaryKey
 
 @Entity(tableName = "costs")
 data class CostEntity(
-    @PrimaryKey(autoGenerate = true) val id: Int = 0,
-    val installation: Long = 0L,
-    val welding: Long = 0L,
-    val transport: Long = 0L
+    @PrimaryKey(autoGenerate = true) var id: Int = 0,
+    var installation: Long = 0L,
+    var welding: Long = 0L,
+    var transport: Long = 0L
 )
