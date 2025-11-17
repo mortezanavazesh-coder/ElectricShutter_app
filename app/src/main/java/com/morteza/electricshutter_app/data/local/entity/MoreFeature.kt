ab18eb9b7d35cfa103b3d0425a6c63ceb19e0178
mortezanavazesh-coder <morteza.navazesh@gmail.com>
2025-11-17 18:37:55 +0330

    Update MoreFeature.kt

--- app/src/main/java/com/morteza/electricshutter_app/data/local/entity/MoreFeature.kt
+++ app/src/main/java/com/morteza/electricshutter_app/data/local/entity/MoreFeature.kt
@@ -5,7 +5,7 @@ import androidx.room.PrimaryKey
 
 @Entity(tableName = "more_features")
 data class MoreFeature(
-    @PrimaryKey(autoGenerate = true) val id: Int = 0,
-    val name: String,
-    val price: Long
+    @PrimaryKey(autoGenerate = true) var id: Int = 0,
+    var name: String,
+    var price: Long
 )
