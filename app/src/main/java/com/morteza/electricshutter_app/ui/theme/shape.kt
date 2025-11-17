4b6cbe27ec49ff67d831654edb558f6704f0007f
mortezanavazesh-coder <morteza.navazesh@gmail.com>
2025-11-17 06:52:42 +0330

    Update shape.kt

--- app/src/main/java/com/morteza/electricshutter_app/ui/theme/shape.kt
+++ app/src/main/java/com/morteza/electricshutter_app/ui/theme/shape.kt
@@ -1,11 +1,11 @@
-package com.morteza.shuttercalculator.shuttercalculator.ui.theme
+package com.morteza.electricshutter_app.ui.theme
 
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Shapes
 import androidx.compose.ui.unit.dp
 
 val AppShapes = Shapes(
-    small = RoundedCornerShape(8.dp),
-    medium = RoundedCornerShape(12.dp),
-    large = RoundedCornerShape(16.dp)
+    small = RoundedCornerShape(4.dp),
+    medium = RoundedCornerShape(8.dp),
+    large = RoundedCornerShape(12.dp)
 )
