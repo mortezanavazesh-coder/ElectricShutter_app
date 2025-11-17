2f2bc0b04d101630abb23d010abeaff151a03c1a
mortezanavazesh-coder <morteza.navazesh@gmail.com>
2025-11-17 12:12:09 +0330

    Update shape.kt

--- app/src/main/java/com/morteza/electricshutter_app/ui/theme/shape.kt
+++ app/src/main/java/com/morteza/electricshutter_app/ui/theme/shape.kt
@@ -6,6 +6,6 @@ import androidx.compose.ui.unit.dp
 
 val AppShapes = Shapes(
     small = RoundedCornerShape(4.dp),
-    medium = RoundedCornerShape(8.dp),
-    large = RoundedCornerShape(12.dp)
+    medium = RoundedCornerShape(10.dp),
+    large = RoundedCornerShape(20.dp)
 )
