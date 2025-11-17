65c5156d1be4f402859305fa150a2661b3360015
mortezanavazesh-coder <morteza.navazesh@gmail.com>
2025-11-17 18:21:46 +0330

    Update CostDao.kt

--- app/src/main/java/com/morteza/electricshutter_app/data/db/dao/CostDao.kt
+++ app/src/main/java/com/morteza/electricshutter_app/data/db/dao/CostDao.kt
@@ -1,16 +1,15 @@
 package com.morteza.electricshutter_app.data.db.dao
 
 import androidx.room.*
-import com.electricshutter_app.data.models.CostEntity
+import com.morteza.electricshutter_app.data.local.entity.CostEntity
 import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface CostDao {
 
     @Query("SELECT * FROM costs LIMIT 1")
-    fun getCosts(): Flow<CostEntity>
+    fun getCosts(): Flow<CostEntity?>
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertOrUpdate(cost: CostEntity)
-
 }
