db8ae2563d379b990826ef42f7ecbf8ea6ca07bc
mortezanavazesh-coder <morteza.navazesh@gmail.com>
2025-11-17 17:18:55 +0330

    Update AppDatabase.kt

--- app/src/main/java/com/morteza/electricshutter_app/data/db/AppDatabase.kt
+++ app/src/main/java/com/morteza/electricshutter_app/data/db/AppDatabase.kt
@@ -3,20 +3,27 @@ package com.morteza.electricshutter_app.data.db
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
+
+import com.morteza.electricshutter_app.data.local.entity.CustomerReportEntity
+import com.morteza.electricshutter_app.data.local.entity.MoreFeature
 import com.morteza.electricshutter_app.data.local.entity.CostEntity
-import com.morteza.electricshutter_app.data.models.CustomerReportEntity
-import com.morteza.electricshutter_app.data.models.MoreFeature
+
 import com.morteza.electricshutter_app.data.db.dao.MoreFeatureDao
 import com.morteza.electricshutter_app.data.db.dao.CostDao
 import com.morteza.electricshutter_app.data.db.dao.CustomerReportDao
 
 @Database(
-    entities = [CustomerReportEntity::class, MoreFeature::class, CostEntity::class],
+    entities = [
+        CustomerReportEntity::class,
+        MoreFeature::class,
+        CostEntity::class
+    ],
     version = 1,
     exportSchema = false
 )
 @TypeConverters(Converters::class)
 abstract class AppDatabase : RoomDatabase() {
+
     abstract fun moreFeatureDao(): MoreFeatureDao
     abstract fun costDao(): CostDao
     abstract fun customerReportDao(): CustomerReportDao
